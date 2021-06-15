7187c9bed48fc5ae04046cc739ff35a7d4af4624
Mateusz Olszak <mateuszolszak98@gmail.com>
2021-06-16 01:45:52 +0200

    Module 7.2, ImmutableForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,18 +1,21 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.beautifier.PoemDecorator;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
     public static void main(String[] args) {
+//
+//        String text = "This is my examplary sentence.";
+//        String decoratedText1 = text.toUpperCase().replaceAll("[E ]","*").split("X")[0];
+//        String decoratedText2 = text.toUpperCase().replaceAll("[E ]","*").split("X")[1];
+//
+//        PoemBeautifier.beautify(text, decorator -> "ABC | " + decoratedText1 + " | ABC" + "!!!");
+//        PoemBeautifier.beautify(text, decorator -> "ABC | " + decoratedText2 + " | ABC" + "!!!");
 
-        String text = "This is my examplary sentence.";
-        String decoratedText1 = text.toUpperCase().replaceAll("[E ]","*").split("X")[0];
-        String decoratedText2 = text.toUpperCase().replaceAll("[E ]","*").split("X")[1];
+        NumbersGenerator.generateEven(20);
 
-        PoemBeautifier.beautify(text, decorator -> "ABC | " + decoratedText1 + " | ABC" + "!!!");
-        PoemBeautifier.beautify(text, decorator -> "ABC | " + decoratedText2 + " | ABC" + "!!!");
     }
 
 }
