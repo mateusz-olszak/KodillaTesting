26ee45e64815ce0c2cee5a1c4727730408499bfa
Mateusz Olszak <mateuszolszak98@gmail.com>
2021-06-17 02:39:54 +0200

    Module 7.5, IntStream calculate mean average with assertions

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -4,11 +4,13 @@ import java.util.OptionalDouble;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-    static void getAverage(int[] numbers){
+    static double getAverage(int[] numbers){
         IntStream.range(0, numbers.length).map(n -> numbers[n]).forEach(System.out::println);
 
         double average = IntStream.range(0, numbers.length).map(n -> numbers[n]).average().getAsDouble();
 
         System.out.println("Average: " + average);
+
+        return average;
     }
 }
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -2,13 +2,15 @@ package com.kodilla.stream.array;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class ArrayOperationsTestSuite {
 
     @Test
     public void testGetAverage(){
         int[] numbers = {2,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15,15,16,15,15};
 
-        ArrayOperations.getAverage(numbers);
+        assertEquals(9.85, ArrayOperations.getAverage(numbers));
     }
 
 }
