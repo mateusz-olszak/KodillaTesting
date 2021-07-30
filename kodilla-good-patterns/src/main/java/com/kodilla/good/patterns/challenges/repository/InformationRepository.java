ec415686144cc4fe73bc2265bebcb8c7ecef8853
Mateusz Olszak <mateuszolszak98@gmail.com>
2021-07-30 10:40:03 +0200

    Task 13.2

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/repository/InformationRepository.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/repository/InformationRepository.java
@@ -3,6 +3,6 @@ package com.kodilla.good.patterns.challenges.repository;
 import com.kodilla.good.patterns.challenges.data.Product;
 import com.kodilla.good.patterns.challenges.data.User;
 
-public interface InformationService {
+public interface InformationRepository {
     void inform(User user, Product product);
 }
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/service/ProductOrderService.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/service/ProductOrderService.java
@@ -2,17 +2,17 @@ package com.kodilla.good.patterns.challenges.service;
 
 import com.kodilla.good.patterns.challenges.data.OrderDto;
 import com.kodilla.good.patterns.challenges.data.OrderRequest;
-import com.kodilla.good.patterns.challenges.repository.InformationService;
+import com.kodilla.good.patterns.challenges.repository.InformationRepository;
 import com.kodilla.good.patterns.challenges.repository.OrderDaoRepository;
 import com.kodilla.good.patterns.challenges.repository.OrderRepository;
 
 public class ProductOrderService {
-    private InformationService informationService;
+    private InformationRepository informationRepository;
     private OrderRepository orderRepository;
     private OrderDaoRepository orderDaoRepository;
 
-    public ProductOrderService(InformationService informationService, OrderRepository orderRepository, OrderDaoRepository orderDaoRepository) {
-        this.informationService = informationService;
+    public ProductOrderService(InformationRepository informationService, OrderRepository orderRepository, OrderDaoRepository orderDaoRepository) {
+        this.informationRepository = informationService;
         this.orderRepository = orderRepository;
         this.orderDaoRepository = orderDaoRepository;
     }
@@ -22,7 +22,7 @@ public class ProductOrderService {
         boolean isOrdered = orderRepository.makeOrder(orderRequest.getUser(), orderRequest.getProduct());
 
         if (isOrdered){
-            informationService.inform(orderRequest.getUser(),orderRequest.getProduct());
+            informationRepository.inform(orderRequest.getUser(),orderRequest.getProduct());
             orderDaoRepository.createOrder(orderRequest.getUser(), orderRequest.getProduct());
             return new OrderDto(orderRequest.getUser(), orderRequest.getProduct(), true);
         }else {
